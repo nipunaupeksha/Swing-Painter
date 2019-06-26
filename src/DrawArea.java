e5988226667127f169a7d686ea4c4e63993dea69
nipunaupeksha <nipuna.upeksha@gmail.com>
2019-06-26 12:45:09 +0530

    second commit

---
+++ src/DrawArea.java
@@ -0,0 +1,106 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import javax.swing.JComponent;
+
+/**
+ * Component for drawing
+ *
+ * @author Nipuna
+ */
+public class DrawArea extends JComponent {
+    //Image is which we're going to draw
+
+    private Image image;
+    //Graphics2D object ==> used to draw on
+    private Graphics2D g2=null;
+    //Mouse coordinates
+    private int currentX, currentY, oldX, oldY;
+
+    public DrawArea() {
+        setDoubleBuffered(false);
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                //save coordinates x,y when mouse is pressed
+                oldX = e.getX();
+                oldY = getY();
+            }
+        });
+        addMouseMotionListener(new MouseMotionAdapter() {
+            //coordinates of x,y when drag mouse
+            public void mouseDragged(MouseEvent e) {
+                //draw line ar g2 context not null
+                currentX = e.getX();
+                currentY = e.getY();
+                if (g2 != null) {
+                    g2.drawLine(oldX, oldY, currentX, currentX);
+                    //refresh draw area to repaint
+                    repaint();
+                    //store current coordinates as old coordinates
+                    oldX = currentX;
+                    oldY = currentY;
+                }
+            }
+        });
+    }
+
+    protected void paintComponent(Graphics g) {
+        if (image != null) {
+            //image to draw null ==> we create
+            image = createImage(getSize().width, getSize().height);
+            g2 = (Graphics2D) image.getGraphics();
+            //enable antialiasing
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            //clear draw area
+            clear();
+
+            g.drawImage(image, 0, 0, null);
+        }
+
+    }
+
+    //now we create exposed methods
+    public void clear() {
+        g2.setPaint(Color.white);
+        //draw white on entire draw area to clear
+        g2.fillRect(0, 0, getSize().width, getSize().height);
+        g2.setPaint(Color.black);
+        repaint();
+    }
+
+    public void red() {
+        //apply red color on g2 context
+        g2.setPaint(Color.red);
+    }
+
+    public void black() {
+        //apply black color on g2 context
+        g2.setPaint(Color.black);
+    }
+
+    public void magenta() {
+        //apply magenta color on g2 context
+        g2.setPaint(Color.magenta);
+    }
+
+    public void green() {
+        //apply green color on g2 context
+        g2.setPaint(Color.green);
+    }
+
+    public void blue() {
+        //apply blue color on g2 context
+        g2.setPaint(Color.blue);
+    }
+}
---
+++ src/SwingPaint.java
@@ -0,0 +1,94 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Nipuna
+ */
+public class SwingPaint {
+
+    JButton clearBtn = new JButton("Clear");
+    JButton blackBtn = new JButton("Black");
+    JButton blueBtn = new JButton("Blue");
+    JButton greenBtn = new JButton("Green");
+    JButton redBtn = new JButton("Red");
+    JButton magentaBtn = new JButton("Magenta");
+    DrawArea drawArea =new DrawArea();
+    ActionListener actionListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == clearBtn) {
+                drawArea.clear();
+            } else if (e.getSource() == blackBtn) {
+                drawArea.black();
+            } else if (e.getSource() == greenBtn) {
+                drawArea.green();
+            } else if (e.getSource() == redBtn) {
+                drawArea.red();
+            } else if (e.getSource() == blueBtn) {
+                drawArea.blue();
+            } else if (e.getSource() == magentaBtn) {
+                drawArea.magenta();
+            }
+        }
+    };
+
+    public static void main(String args[]) {
+        new SwingPaint().show();
+    }
+
+    public void show() {
+        JFrame frame = new JFrame("Swing Paint");
+        Container content = frame.getContentPane();
+        //set layout on ccontent pane
+        content.setLayout(new BorderLayout());
+        //create draw area
+        final DrawArea drawArea = new DrawArea();
+        //add to content pane
+        content.add(drawArea, BorderLayout.CENTER);
+        //create controls to apply and call clear feature
+        JPanel controls = new JPanel();
+
+        clearBtn.addActionListener(actionListener);
+
+        blackBtn.addActionListener(actionListener);
+
+        blueBtn.addActionListener(actionListener);
+
+        greenBtn.addActionListener(actionListener);
+
+        redBtn.addActionListener(actionListener);
+
+        magentaBtn.addActionListener(actionListener);
+
+        //add to panel
+        controls.add(greenBtn);
+        controls.add(blueBtn);
+        controls.add(blackBtn);
+        controls.add(redBtn);
+        controls.add(magentaBtn);
+        controls.add(clearBtn);
+
+        //add to content pane
+        content.add(controls, BorderLayout.NORTH);
+
+        frame.setSize(600, 600);
+        //can close frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //set location
+        frame.setLocationRelativeTo(null);
+        //show the swing paint result
+        frame.setVisible(true);
+
+    }
+}
